ef5a2fb0ba6f7c1263a96da32d90f465179c01c3
Badru <badruakfm@gmail.com>
2020-06-05 21:05:18 +0700

    1

---
+++ task1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+
+using namespace std;
+
+int sorting(int data[], int size);
+
+main(){
+	int jumlah, min, temp;
+	cout<<"Task 1\n\n\n";
+	cout<<"Masukan jumlah data : ";
+	cin>>jumlah;
+	int data[jumlah];
+	
+	
+	for(int i=1; i<=jumlah; i++){
+		cout<<"Masukan data ke-"<<i<<" : ";
+		cin>>data[i-1];
+	}
+	cout<<"\n\n";
+	for(int i=0; i<jumlah; i++){
+		cout<<data[i]<<" ";
+	}
+	
+	cout<<"\n\n";
+	
+	for(int i=jumlah-1; i>=0; i--){
+		min = i; 
+		for(int j=0; j<i; j++){
+			if(data[j] < data[i]){
+				min = j;
+			}
+		}
+		temp = data[min];
+		data[min] = data[i];
+		data[i] = temp;
+	}
+	
+	for(int i=0; i<jumlah; i++){
+		cout<<data[i]<<" ";
+	}
+	cout<<"\n";
+	cout<<"Data terbesar kedua adalah : "<<data[1]<<"\n\n\n";
+	system("pause");
+	getch();
+}
